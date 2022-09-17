f6a825b0dadd4bf081cbf296eb02e24a2403148f
Sourav Khare <souravkhare34@gmail.com>
2022-09-17 12:19:11 +0530

    [Sourav]- Program to show if the flipping coin game tie then run till difference of minimum 2 points is achieved

---
+++ flipcoinSimulator3.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+isHead=1;
+h=0;
+t=0;
+function flipcoin()
+{
+	read -p "Enter no of flips: " f;
+for (( i=1 ; i<=$f ; i++ ))
+
+do
+
+c=$(( RANDOM%2 ))
+
+if [ $isHead -eq $c ]
+then
+	a=head;
+	(( h++ ))
+else
+	a=tail;
+	(( t++ ))
+fi
+done
+
+if [ $h -ge 21 ]
+then
+echo "Head won $h times and by $(( $h - $t )) times" ;
+
+elif [ $t -ge 21 ]
+then
+echo "Tail won $t times and by $(( $t -$h )) times";
+
+elif [ $h -eq $t ]
+then
+
+	echo " It's a tie";
+	echo " head: $h";
+	echo " tail: $t";
+
+	flipcoin;
+
+
+else
+	echo "Head won $h times";
+	echo "Tail won $t times";
+
+fi
+
+}
+
+
+
+	flipcoin ;
+
+
+
